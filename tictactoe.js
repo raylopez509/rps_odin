94b78c23a856ff795891474d67b5a6b5a8ff007b
Ray Lopez <raylopez509@gmail.com>
2024-12-23 22:59:27 -0800

    Created getComputerChoice function. getComputerChoice gets a random number between 0-2, which will return a choice of rock, paper, and scissors.

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,15 @@
-console.log("hi");
+function getComputerChoice() {
+  randnum = Math.floor(Math.random() * 3);
+  switch (randnum) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
+
+for (i = 0; i < 100; i++) {
+  console.log(getComputerChoice());
+}
