c122fe97430f625b48eb3f19e5826f4b7066412c
Ray Lopez <raylopez509@gmail.com>
2024-12-23 23:20:35 -0800

    created playRound function. playRound takes in two parameters, humanChoice and computerChoice. It will decide who wins based on the choices and increment the score for that player.

--- tictactoe.js
+++ tictactoe.js
@@ -22,3 +22,26 @@ function getHumanChoice() {
 
 humanScore = 0;
 computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    console.log("You win! " + humanChoice + " beats " + computerChoice);
+    humanScore++;
+  } else if (
+    (humanChoice == "rock" && computerChoice == "paper") ||
+    (humanChoice == "paper" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "rock")
+  ) {
+    console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    computerScore++;
+  }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
